482b3d2bd74aaf37b1e095adf994804a1200c769
Wenderson <wps713@gmail.com>
2019-10-30 20:06:03 -0300

    Setting up html

--- public/bundle.js
+++ public/bundle.js
@@ -93,7 +93,7 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-eval("\n\n//# sourceURL=webpack:///./src/index.js?");
+eval("alert(\"Hello world\");\n\n//# sourceURL=webpack:///./src/index.js?");
 
 /***/ })
 
