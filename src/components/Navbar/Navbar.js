70bfa2e5d2f4cb2d5a577b11320bc12bb325a137
Wenderson <wps713@gmail.com>
2019-10-31 16:14:16 -0300

    Adding navbar css and importing in component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import facebookLogo from '../../assets/facebook.svg';
+import './Navbar.css'
 
 export default function Navbar() {
   return (
-    <div>
+    <div className="navbarContainer">
       <img src={facebookLogo} alt="Facebook logo" />
     </div>
   );
