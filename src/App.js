6a04b4444c40d0d00a8db9b80919739e87cae091
Wenderson <wps713@gmail.com>
2019-10-31 16:07:26 -0300

    Adding Navbar component and importing in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
-
+import Navbar from './components/Navbar';
 function App() {
-  return <h1>Hello RocketSeat</h1>
+  return <Navbar />
 }
 
 export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import facebookLogo from '../../assets/facebook.svg';
+
+export default function Navbar() {
+  return (
+    <div>
+      <img src={facebookLogo} alt="Facebook logo" />
+    </div>
+  );
+}
