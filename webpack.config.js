939f8cad8947100cad049b6dc085804c93d70a63
Wenderson <wps713@gmail.com>
2019-10-31 16:02:51 -0300

    Adding svg reader in webpack and adding logo.svg

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
         }
       },
       {
-        test: /.*\.(gif|png|jpe?g)$/i,
+        test: /.*\.(svg|gif|png|jpe?g)$/i,
         use: {
           loader: 'file-loader'
         }
