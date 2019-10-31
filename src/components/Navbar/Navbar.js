01d75d94c6f86f5c6fae6e88acc292948f5bab2f
Wenderson <wps713@gmail.com>
2019-10-31 16:41:09 -0300

    Completing navbar itens

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import facebookLogo from '../../assets/facebook.svg';
+import userImage from '../../assets/user.png'
 import './Navbar.css'
 
 export default function Navbar() {
   return (
     <div className="navbarContainer">
       <img src={facebookLogo} alt="Facebook logo" />
+      <h5>Meu perfil</h5>
+      <img src={userImage} alt="User logo" />
     </div>
   );
 }
