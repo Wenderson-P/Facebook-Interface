afd7afee257d60437da006d19021e877b7c21e85
Wenderson <wps713@gmail.com>
2019-11-02 18:02:57 -0300

    Adding Post style, changing post tags

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import PostHeader from '../PostHeader/PostHeader';
-
+import './Post.css'
 export default function Post({ key, data }) {
   return (
-    <div>
-      <PostHeader author={data.author} date={data.date} />
+    <div className="postContainer">
+      <div className="postData">
+        <PostHeader
+          author={data.author}
+          date={data.date}
+          content={data.content} />
+        <div className="split"></div>
+      </div>
     </div>
   );
 }
