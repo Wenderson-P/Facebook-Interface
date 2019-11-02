16cfb995a68427b1d5c7ef446eda5ca867995fc4
Wenderson <wps713@gmail.com>
2019-11-02 11:55:14 -0300

    Calling postList in Main page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar';
+import PostList from './components/PostList/PostList'
 function App() {
   return (
     <>
       <Navbar />
+      <PostList />
     </>
   )
 }
