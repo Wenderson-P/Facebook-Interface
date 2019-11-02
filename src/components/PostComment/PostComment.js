860b6e5461c2c6086c7fc9c2f36a756c39ace28a
Wenderson <wps713@gmail.com>
2019-11-02 18:29:59 -0300

    Changing comment to content in params

--- src/components/PostComment/PostComment.js
+++ src/components/PostComment/PostComment.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-export default function PostComment({ key, author, comment }) {
+export default function PostComment({ key, author, content }) {
   return (
     <div className="postCommentContainer">
       <img src={author.avatar} alt="avatar usuario" />
       <div className="comment">
         <text>
-          <span>{author.name}</span>
+          <span>{author.name} </span>
           {content}
         </text>
       </div>
