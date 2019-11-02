57c31687d3bcd0404c7333f1f075171bacfbe29e
Wenderson <wps713@gmail.com>
2019-11-02 18:13:01 -0300

    Adding PostComment component

---
+++ src/components/PostComment/PostComment.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function PostComment({ key, author, comment }) {
+  return (
+    <div className="postCommentContainer">
+      <img src={author.avatar} alt="avatar usuario" />
+      <div className="comment">
+        <text>
+          <span>{author.name}</span>
+          {content}
+        </text>
+      </div>
+    </div>
+  );
+}
