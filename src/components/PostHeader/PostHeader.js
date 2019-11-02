ab3b9b21c35271560d4ca89147a64e17fc8a8371
Wenderson <wps713@gmail.com>
2019-11-02 12:25:21 -0300

    Adding post Header date, content and adding classname

--- src/components/PostHeader/PostHeader.js
+++ src/components/PostHeader/PostHeader.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import './PostHeader.css'
 
-export default function PostHeader({ author }) {
+export default function PostHeader({ author, date, content }) {
   return (
-    <div>
-      <img src={author.avatar} alt='autor avatar' />
-      <h4>
-        {author.name}
-      </h4>
+    <div className="postHeaderContainer">
+      <div className="authorContainer">
+        <img src={author.avatar} alt='autor avatar' className="authorImg" />
+        <h4>
+          {author.name}
+        </h4>
+      </div>
+      <h5 >{date}</h5>
+      <div>
+        {content}
+      </div>
     </div>
   );
 }
