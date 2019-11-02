d6557e219bd65e1b8ca5d6028e324a17e147f90d
Wenderson <wps713@gmail.com>
2019-11-02 18:34:46 -0300

    Adding PostComment in Post

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PostHeader from '../PostHeader/PostHeader';
+import PostComment from '../PostComment/PostComment'
 import './Post.css'
 export default function Post({ key, data }) {
   return (
@@ -10,6 +11,13 @@ export default function Post({ key, data }) {
           date={data.date}
           content={data.content} />
         <div className="split"></div>
+        {data.comments.map(comment =>
+          <PostComment
+            key={comment.id}
+            author={comment.author}
+            content={comment.content}
+          />
+        )}
       </div>
     </div>
   );
