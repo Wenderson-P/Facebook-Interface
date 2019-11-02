737bbf14e96f12374518173a57be3f6a1ca48bbe
Wenderson <wps713@gmail.com>
2019-11-02 11:54:54 -0300

    Changing state calling mode because of offline development
    I'm developing without internet and i need to download one babel plugin for the old state mode to work, so i'm changing to the 'classic' mode, inside a constructor.

--- src/components/PostList/PostList.js
+++ src/components/PostList/PostList.js
@@ -2,32 +2,34 @@ import React, { Component } from 'react';
 import Post from '../Post/Post'
 
 export default class PostList extends Component {
-  state = {
-    posts: [
-      {
-        id: 1,
-        author: {
-          name: "Claire Watts",
-          avatar: "https://randomuser.me/api/portraits/women/8.jpg"
-        },
-        date: "04 Jun 2019",
-        content: "Guys, anyone knows how to buy a ticket for the Tokyo Olympics",
-        comments: [
-          {
-            id: 1,
-            author: {
-              name: "Jon Steeves",
-              avatar: "https://randomuser.me/api/portraits/men/31.jpg"
-            },
-            content: "Hey, take a look at this link https://tokyo2020.org/en/games/ticket/ :)"
-          }
-        ]
-      },
-      {
-      }
-    ]
-  };
 
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [
+        {
+          id: 1,
+          author: {
+            name: "Claire Watts",
+            avatar: "https://randomuser.me/api/portraits/women/8.jpg"
+          },
+          date: "04 Jun 2019",
+          content: "Guys, anyone knows how to buy a ticket for the Tokyo Olympics",
+          comments: [
+            {
+              id: 1,
+              author: {
+                name: "Jon Steeves",
+                avatar: "https://randomuser.me/api/portraits/men/31.jpg"
+              },
+              content: "Hey, take a look at this link https://tokyo2020.org/en/games/ticket/ :)"
+            }
+          ]
+        },
+      ]
+    }
+  }
   render() {
     const { posts } = this.state
     return (
